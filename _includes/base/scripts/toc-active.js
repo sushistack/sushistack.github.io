099a2742368698c908fba9103b367f96d0fcbbda
eli <purple.melon@outlook.com>
2021-05-10 20:41:56 +0900

    updates

--- _includes/base/scripts/toc-active.js
+++ _includes/base/scripts/toc-active.js
@@ -1,16 +1,28 @@
 var initToc = function () {
   var observer = new IntersectionObserver(function (entries) {
-    entries.forEach(entry => {
-      var cur = document.querySelector(`.toc-entry a[href="#${entry.target.id}"`)
-      if (cur) {
-        if (entry.intersectionRatio > 0) {
-          cur.classList.add('active');
-        } else {
-          cur.classList.remove('active');
-        }
-      }
-    });
+    entries.forEach(() => activateHeading());
   });
+
+  var activateHeading = function () {
+    var lastActive = document.querySelector(`.toc-entry a.active`);
+    var headings = document.querySelectorAll(selector)
+
+    var hRect, curActive, changed = true;
+    var shRect = document.querySelector('header.page-header').getBoundingClientRect();
+    for (var i = headings.length - 1; i > -1; i -= 1) {
+      hRect = headings[i].getBoundingClientRect();
+      shHeight = Math.floor(shRect.top) + Math.floor(shRect.height) + 20;
+      if (hRect.top <= shHeight) {
+        curActive = document.querySelector(`.toc-entry a[href="#${headings[i].id}"]`);
+        curActive.classList.add('active');
+        changed = curActive !== lastActive
+        break;
+      }
+    }
+    if (changed && lastActive) {
+      lastActive.classList.remove('active')
+    }
+  }
 
   var cb = '.content-body';
   var selector = `${cb} h1, ${cb} h2, ${cb} h3, ${cb} h4, ${cb} h5, ${cb} h6`;
